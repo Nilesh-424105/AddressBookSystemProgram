5adbd16a768918a4b9eb1eec446f4f1e479d01c0
Nilesh-424105 <89961130+Nilesh-424105@users.noreply.github.com>
2021-11-18 16:21:10 +0530

    ADDED: To Create a Contact In AddressBook

---
+++ com/addressbook/ContactStore.java
@@ -0,0 +1,18 @@
+package com.addressbook;
+
+import java.util.ArrayList;
+
+public class ContactStore {
+	
+	private ArrayList<AddressBook> contactList = new ArrayList<AddressBook>();
+	
+	public ArrayList<AddressBook> getContactList() {
+		return contactList;
+		
+	}
+	
+	public void add(AddressBook contact) {
+		contactList.add(contact);
+	}
+
+}
