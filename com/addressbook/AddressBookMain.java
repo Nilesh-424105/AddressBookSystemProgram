9b2b3564a8db094b28cb5abb142709e465d4df95
Nilesh-424105 <89961130+Nilesh-424105@users.noreply.github.com>
2021-11-18 16:42:32 +0530

    ADDED: To Add NewContact In Address Book

--- com/addressbook/AddressBookMain.java
+++ com/addressbook/AddressBookMain.java
@@ -7,19 +7,11 @@ public class AddressBookMain {
 		System.out.println("Welcome to Address Book Program");
 		
 		AddressBook contact = new AddressBook();
-		contact.setFirstName("Nilesh");
-		contact.setLastName("Kotkar");
-		contact.setAddress("12/15, abcd colony, efgh nagar");
-		contact.setCity("Jalgaon");
-		contact.setState("Maharastra");
-		contact.setZip("425215");
-		contact.setPhoneNumber("8766483622");
-		contact.setEmail("nilesh4526@gmail.com");
-		
 		ContactStore contactStore = new ContactStore();
 		contactStore.add(contact);
 		
 		UserInterface userInterface = new UserInterface();
+		userInterface.addNewContact(contact);
 		userInterface.print(contactStore.getContactList());
 	}
 }
--- com/addressbook/UserInterface.java
+++ com/addressbook/UserInterface.java
@@ -1,6 +1,7 @@
 package com.addressbook;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class UserInterface {
 	
@@ -12,4 +13,24 @@ public class UserInterface {
 		}
 	}
 
+	public void addNewContact(AddressBook contactThree) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter First Name");
+		contactThree.setFirstName(sc.nextLine());
+		System.out.println("Enter Last Name");
+		contactThree.setLastName(sc.nextLine());
+		System.out.println("Enter Address");
+		contactThree.setAddress(sc.nextLine());
+		System.out.println("Enter City");
+		contactThree.setCity(sc.nextLine());
+		System.out.println("Enter State");
+		contactThree.setState(sc.nextLine());
+		System.out.println("Enter Zip Code");
+		contactThree.setZip(sc.nextLine());
+		System.out.println("Enter PhoneNumber");
+		contactThree.setPhoneNumber(sc.nextLine());
+		System.out.println("Enter Email");
+		contactThree.setEmail(sc.nextLine());
+		sc.close();
+	}
 }
