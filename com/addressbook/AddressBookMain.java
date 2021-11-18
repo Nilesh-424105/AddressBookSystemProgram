27a5531f1a10ec8a190d30701e9b71a61ea034c5
Nilesh-424105 <89961130+Nilesh-424105@users.noreply.github.com>
2021-11-18 17:40:05 +0530

    ADDED: Ability to Edit Existing Contact Person

--- com/addressbook/AddressBookMain.java
+++ com/addressbook/AddressBookMain.java
@@ -1,5 +1,7 @@
 package com.addressbook;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
 
 	public static void main(String[] args) {
@@ -7,11 +9,43 @@ public class AddressBookMain {
 		System.out.println("Welcome to Address Book Program");
 		
 		AddressBook contact = new AddressBook();
+		contact.setFirstName("Nilesh");
+		contact.setLastName("Kotkar");
+		contact.setAddress("12/15, abcd colony, efgh nagar");
+		contact.setCity("Jalgaon");
+		contact.setState("Maharastra");
+		contact.setZip("425215");
+		contact.setPhoneNumber("8766483622");
+		contact.setEmail("nilesh4526@gmail.com");
+		
 		ContactStore contactStore = new ContactStore();
 		contactStore.add(contact);
 		
 		UserInterface userInterface = new UserInterface();
-		userInterface.addNewContact(contact);
-		userInterface.print(contactStore.getContactList());
+		Scanner sc = new Scanner(System.in);
+		boolean check = true;
+		while (check) {
+			System.out.println("Enter choise 1.Add the new contact\n 2.Edit Existing Contact\n 3.exit");
+			int choise = sc.nextInt();
+			switch(choise) {
+			case 1:
+				AddressBook newContact = new AddressBook();
+				userInterface.addNewContact(newContact);
+				contactStore.add(newContact);
+				System.out.println("Contact List After Add");
+				userInterface.print(contactStore.getContactList());
+				
+			case 2:
+				contactStore.edit();
+				System.out.println("Contact List After Edit");
+				userInterface.print(contactStore.getContactList());
+				
+			case 3:
+				check = false;
+			}
+			
+				
+				
+		}
 	}
 }
--- com/addressbook/ContactStore.java
+++ com/addressbook/ContactStore.java
@@ -1,11 +1,12 @@
 package com.addressbook;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ContactStore {
 	
 	private ArrayList<AddressBook> contactList = new ArrayList<AddressBook>();
-	
+	Scanner sc = new Scanner(System.in);
 	public ArrayList<AddressBook> getContactList() {
 		return contactList;
 		
@@ -13,6 +14,66 @@ public class ContactStore {
 	
 	public void add(AddressBook contact) {
 		contactList.add(contact);
+	}
+
+	public void edit() {
+		System.out.println("Enter name of contact you want to sell");
+		String name = sc.nextLine();
+		for(AddressBook addressBook : contactList) {
+			if(addressBook.getFirstName().equalsIgnoreCase(name)) {
+				boolean check = true;
+				while(check) {
+					System.out.println("Edit Options 1.FirstName 2.LastName 3.Address 4.City 5.State 6.PhoneNumber 7.Email");
+					int choise = sc.nextInt();
+					sc.nextLine();
+					switch(choise) {
+					case 1:
+						System.out.println("Enter your new FirstName:");
+						String firstName = sc.nextLine();
+						addressBook.setFirstName(firstName);
+						
+					case 2:
+						System.out.println("Enter your new Last Name:");
+						String lastName = sc.nextLine();
+						addressBook.setLastName(lastName);
+						
+					case 3:
+						System.out.println("Enter your new Address:");
+						String address = sc.nextLine();
+						addressBook.setAddress(address);
+						
+					case 4:
+						System.out.println("Enter your new City:");
+						String city = sc.nextLine();
+						addressBook.setCity(city);
+						
+					case 5:
+						System.out.println("Enter your new State:");
+						String state = sc.nextLine();
+						addressBook.setState(state);
+						
+					case 6:	
+						System.out.println("Enter your new Zip:");
+						String zip = sc.nextLine();
+						addressBook.setZip(zip);
+						
+					case 7:
+						System.out.println("Enter your new PhoneNumber:");
+						String phoneNumber = sc.nextLine();
+						addressBook.setPhoneNumber(phoneNumber);
+						
+					case 8:
+						System.out.println("Enter your new Email:");
+						String email = sc.nextLine();
+						addressBook.setEmail(email);
+						
+					default:
+						check = false;
+					}
+				}
+			}
+		}
+		
 	}
 
 }
