1acdb164f8091824f631ca72e26793d017736e85
Nilesh-424105 <89961130+Nilesh-424105@users.noreply.github.com>
2021-11-19 12:27:46 +0530

    ADDED: Ability to view Persons By Cityor State

--- com/addressbook/ContactStore.java
+++ com/addressbook/ContactStore.java
@@ -7,36 +7,36 @@ import java.util.Scanner;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class ContactStore extends AddressBook{
+public class ContactStore extends AddressBook {
 	Scanner sc = new Scanner(System.in);
 
-	 public void setContactDetails() {
-	        System.out.println("enter the First Name");
-	        setFirstName(sc.nextLine());
-	        System.out.println("enter the Last Name");
-	        setLastName(sc.nextLine());
-	        System.out.println("enter the Address Name");
-	        setAddress(sc.nextLine());
-	        System.out.println("Enter city");
-	        setCity(sc.nextLine());
-	        System.out.println("enter the State Name");
-	        setState(sc.nextLine());
-	        System.out.println("enter the Phone Number");
-	        setPhoneNumber(sc.nextLine());
-	        System.out.println("enter the ZipCode");
-	        setZip(sc.nextLine());
-	        System.out.println("enter the Email");
-	        setEmail(sc.nextLine());
-	    }
+	public void setContactDetails() {
+		System.out.println("enter the First Name");
+		setFirstName(sc.nextLine());
+		System.out.println("enter the Last Name");
+		setLastName(sc.nextLine());
+		System.out.println("enter the Address Name");
+		setAddress(sc.nextLine());
+		System.out.println("Enter city");
+		setCity(sc.nextLine());
+		System.out.println("enter the State Name");
+		setState(sc.nextLine());
+		System.out.println("enter the Phone Number");
+		setPhoneNumber(sc.nextLine());
+		System.out.println("enter the ZipCode");
+		setZip(sc.nextLine());
+		System.out.println("enter the Email");
+		setEmail(sc.nextLine());
+	}
 
 	public void editContact(ArrayList<ContactStore> contact) {
 		System.out.println("Enter name of contact to Edit");
 		String name = sc.nextLine();
-		for (int i=0; i<contact.size(); i++) {
-			if (contact.get(i).equals(name)) {
+		for (int i = 0; i < contact.size(); i++) {
+			if (contact.get(i).getFirstName().equals(name)) {
 				boolean check = true;
 				while (check) {
-					System.out.println("Edit Options 1.FirstName 2.LastName 3.Address 4.City 5.State 6.PhoneNumber 7.Email");
+					System.out.println("Enter 1.FirstName 2.LastName 3.Address 4.City 5.State 6.PhoneNumber 7.Email");
 					int choise = sc.nextInt();
 					sc.nextLine();
 					switch (choise) {
@@ -84,7 +84,7 @@ public class ContactStore extends AddressBook{
 						check = false;
 					}
 				}
-			}else
+			} else
 				System.out.println("Your name is not matched");
 		}
 	}
@@ -98,21 +98,33 @@ public class ContactStore extends AddressBook{
 			}
 		}
 	}
+
 	public boolean searchForDuplication(ArrayList<ContactStore> contactStores, ContactStore contact) {
 		boolean check = false;
 		for (ContactStore contactStore : contactStores) {
-			if(contact.getFirstName().equalsIgnoreCase(contactStore.getFirstName()))
+			if (contact.getFirstName().equalsIgnoreCase(contactStore.getFirstName()))
 				check = true;
 		}
 		return check;
 	}
+
 	public static void search(Hashtable<Integer, ArrayList<ContactStore>> dictionary) {
 		System.out.println("Enter State Name");
 		Scanner sc = new Scanner(System.in);
 		String state = sc.nextLine();
-		for (int i = 1; i <= dictionary.size(); i++){
-            List<ContactStore> person = dictionary.get(i).stream().filter(s -> s.getState().equalsIgnoreCase(state)).collect(Collectors.toList());
-            System.out.println(person);
+		System.out.println("Enter City Name");
+		String city = sc.nextLine();
+		System.out.println("Persons with State name: " + state);
+		for (int i = 1; i <= dictionary.size(); i++) {
+			List<ContactStore> personWithState = dictionary.get(i).stream()
+					.filter(s -> s.getState().equalsIgnoreCase(state)).collect(Collectors.toList());
+			System.out.println(personWithState);
+		}
+		System.out.println("Persons with City name: " + city);
+		for (int i = 1; i <= dictionary.size(); i++) {
+			List<ContactStore> personWithCity = dictionary.get(i).stream()
+					.filter(c -> c.getCity().equalsIgnoreCase(city)).collect(Collectors.toList());
+			System.out.println(personWithCity);
 		}
 	}
 }
